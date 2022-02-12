935c1b1e653755593d6e060f3a587d71def96625
molinar1999 <matthew.a.molinar@gmail.com>
2022-02-12 00:42:28 -0600

    make sure tasks not archived before showing in next 7

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -24,5 +24,22 @@ export const useTasks = (selectedTrack) => {
         : selectedTrack === 'INBOX' || selectedTrack === 0
         ? (unsubscribe = unsubscribe.where('date', '==', ''))
         : unsubscribe;
+
+    unsubscribe = unsubscribe.onSnapshot((snapshot) => {
+      const newTasks = snapshot.docs.map((task) => ({
+        id: task.id,
+        ...task.data(),
+      }));
+
+      setTasks(
+        selectedProject === 'NEXT_7'
+          ? newTasks.filter(
+              (task) =>
+                moment(task.date, 'DD-MM-YYYY').diff(moment(), 'days') <= 7 &&
+                task.archived !== true
+            )
+          : newTasks.filter((task) => task.archived !== true)
+      );
+    });
   }, []);
 };
