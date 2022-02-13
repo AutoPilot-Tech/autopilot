465344b30e2e4b880aad3bf9ccf629efa68987f3
molinar1999 <matthew.a.molinar@gmail.com>
2022-02-12 19:15:14 -0600

    track provider - can change to redux later

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Header } from './components/layout/Header';
 import { Content } from './components/layout/Content';
+import { TracksProvider, SelectedTrackProvider } from './context';
 import './App.scss';
 
+// note: see src/context. Since we want to use tracksprovider at the
+// top level, we are using it here in App. This can be replaced
+// with Redux, later.
 export const App = () => {
   return (
-    <div className="App">
-      <Header />
-      <Content />
-    </div>
+    <SelectedTrackProvider>
+      <TracksProvider>
+        <div className="App">
+          <Header />
+          <Content />
+        </div>
+      </TracksProvider>
+    </SelectedTrackProvider>
   );
 };
---
+++ src/context/tracks-context.js
@@ -0,0 +1,25 @@
+import React, { createContext, useContext } from 'react';
+import { useTracks } from '../hooks';
+
+// allows us to pass data down the component, without props.
+
+export const TracksContext = createContext();
+// provider is at the top level, and a consumer is at the
+// bottom level wherever maybe 6 levels deep
+export const TracksProvider = ({ children }) => {
+    const { tracks, setTracks } = useTracks();
+
+    return (
+        <TracksContext.Provider value={{ tracks, setTracks }}>
+            {children}
+        </TracksContext.Provider>
+    );
+};
+
+// this will return the tracks
+export const useTracksValue = () => useContext(TracksContext);
+
+
+
+// example
+// const { tracks } = useProjectsValue();
