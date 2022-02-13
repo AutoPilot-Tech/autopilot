9f13551a4f1c583055e59537bcfd1200d7ff619b
molinar1999 <matthew.a.molinar@gmail.com>
2022-02-13 04:18:26 -0600

    get title helper

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,29 +1,45 @@
-import React from 'react';
-import { collection, getDocs } from "firebase/firestore";
+import React, { useEffect } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
 import { Checkbox } from './Checkbox';
 import { useTasks } from '../hooks';
-
+import { collatedTasks } from '../constants';
+import { getTitle, getCollatedTitle, collatedTasksExist } from '../helpers';
+import { useSelectedTrackValue, useTracksValue } from '../context';
 
 // this just gets the tasks and renders them
 export const Tasks = () => {
-  const { tasks, archivedTasks } = useTasks("1")
+  const { selectedTrack } = useSelectedTrackValue();
+  const { tracks } = useTracksValue();
+  const { tasks } = useTasks(selectedTrack);
 
   let trackName = '';
 
+  if (tracks && selectedTrack && !collatedTasksExist(selectedTrack)) {
+      trackName = getTitle(tracks, selectedTrack).name;
+      console.log('track name 1: ', trackName);
+  }
+
+  if (collatedTasksExist(selectedTrack) && selectedTrack) {
+      trackName = getCollatedTitle(collatedTasks, selectedTrack).name;
+        console.log('track name 2: ', trackName);
+  }
+
+  useEffect(() => {
+      document.title = `${trackName}: Autopilot`
+  },[]);
 
   return (
-      <div className="tasks" data-testid="tasks">
-          <h2 data-testid="track-name">{trackName}</h2>
+    <div className="tasks" data-testid="tasks">
+      <h2 data-testid="track-name">{trackName}</h2>
 
-          <ul className="tasks__list">
-                {tasks.map((task) => (
-                    <li key={`${task.id}`}>
-                        <Checkbox id={task.id} />
-                        <span>{task.task}</span>
-                    </li>
-                ))}
-          </ul>
-      </div>
-  )
-}
-
+      <ul className="tasks__list">
+        {tasks.map((task) => (
+          <li key={`${task.id}`}>
+            <Checkbox id={task.id} />
+            <span>{task.task}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,7 +1,11 @@
 import { collatedTasks } from '../constants';
 
+export const getTitle = (tracks, trackId) => {
+    tracks.find(track => track.trackId === trackId);
+}
+
+
+
 export const collatedTasksExist = selectedTrack => {
     collatedTasks.find(task => task.key === selectedTrack);
-
-
 }
