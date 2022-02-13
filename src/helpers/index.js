9a2c555b1c516248450b98e45ae679b33d987ef0
molinar1999 <matthew.a.molinar@gmail.com>
2022-02-13 04:19:18 -0600

    getcollatedtitle helper

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -4,7 +4,9 @@ export const getTitle = (tracks, trackId) => {
     tracks.find(track => track.trackId === trackId);
 }
 
-
+export const getCollatedTitle = (tracks, key) => {
+    tracks.find(track => track.key === key);
+}
 
 export const collatedTasksExist = selectedTrack => {
     collatedTasks.find(task => task.key === selectedTrack);
