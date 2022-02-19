756410fda2724195ce8c4a02ae1eadfd0826c89e
molinar1999 <matthew.a.molinar@gmail.com>
2022-02-19 00:19:43 -0600

    Dark mode setting

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Header } from './components/layout/Header';
 import { Content } from './components/layout/Content';
 import { TracksProvider } from './context/tracks-context';
@@ -7,13 +7,15 @@ import './App.scss';
 // note: see src/context. Since we want to use tracksprovider at the
 // top level, we are using it here in App. This can be replaced
 // with Redux, later.
-export const App = () => {
+export const App = ({darkModeDefault = true }) => {
+  const [darkMode, setDarkMode] = useState(darkModeDefault);
   return (
       <TracksProvider>
-        <div className="App">
-          <Header />
+        <main data-testid="application"
+        className={darkMode ? 'darkmode' : undefined}>
+          <Header darkMode={darkMode} setDarkMode={setDarkMode} />
           <Content />
-        </div>
+        </main>
        </TracksProvider>
   );
 };
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,21 +1,32 @@
-import React from 'react';
-import { FaMoon } from 'react-icons/fa'
+import React, { useState } from 'react';
+import { FaMoon } from 'react-icons/fa';
 
-export const Header = () => {
+export const Header = ({ darkMode, setDarkMode }) => {
+  const [shouldShowMain, setShouldShowMain] = useState(false);
+  const [showQuickAddTask, setShowQuickAddTask] = useState(false);
+
   return (
     <header className="header" data-testid="header">
       <nav>
         <div className="logo">
           <img src="../../images/logo.png" alt="Autopilot" />
         </div>
-          <div className="settings">
-            <ul>
-              <li className="settings__add" data-testid="quick-add-task-action">+</li>
-              <li className="settings__darkmode" data-testid="dark-mode-action">
-                <FaMoon />
-              </li>
-            </ul>
-          </div>
+        <div className="settings">
+          <ul>
+            <li className="settings__add" data-testid="quick-add-task-action">
+              +
+            </li>
+            <li
+              className="settings__darkmode"
+              data-testid="dark-mode-action"
+              onClick={() => {
+                setDarkMode(!darkMode);
+              }}
+            >
+              <FaMoon />
+            </li>
+          </ul>
+        </div>
       </nav>
     </header>
   );
