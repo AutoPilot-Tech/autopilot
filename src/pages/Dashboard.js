f30e3ae899b2e2e2445ea7fa540889bba38764f2
molinar1999 <matthew.a.molinar@gmail.com>
2022-05-15 01:35:27 -0500

    Add app/calendar/home route

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,24 +1,18 @@
 import React, {useState, useEffect} from "react";
 import AdapterMoment from "@mui/lab/AdapterMoment";
 import {LocalizationProvider} from "@mui/lab";
-
 import {Header} from "../components/layout/Header";
 import {Content} from "../components/layout/Content";
-import {TracksProvider, useTracksValue} from "../context/tracks-context";
-import {Loading} from "./Loading";
+import {TracksProvider} from "../context/tracks-context";
 import {auth, db} from "../firebase";
-import {Scrollbars} from "react-custom-scrollbars";
-import {Footer} from "../components/layout/Footer";
 import SyncLoader from "react-spinners/SyncLoader";
 import {useLoadingValue} from "../context/loading-context";
-import {css} from "@emotion/react";
 import {Banner} from "../components/layout/Banner";
 
 // note: see src/context. Since we want to use tracksprovider at the
 // top level, we are using it here in App. This can be replaced
 // with Redux, later.
-export const Dashboard = ({darkModeDefault = true}) => {
-  const [darkMode, setDarkMode] = useState(darkModeDefault);
+export const Dashboard = () => {
   // const {loading, setLoading} = useLoadingValue();
   const [userLoading, setUserLoading] = useState(true);
   const {setDisplayName, setPhotoUrl} = useLoadingValue();
@@ -57,18 +51,11 @@ export const Dashboard = ({darkModeDefault = true}) => {
   ) : (
     <LocalizationProvider dateAdapter={AdapterMoment}>
       <TracksProvider>
-        {/* <Scrollbars
-          autoHide
-          autoHeight
-          autoHeightMin={0}
-          autoHeightMax={"100vh"}
-        > */}
         <Header />
         <div>
           {showBanner ? <Banner setShowBanner={setShowBanner} /> : null}
           <Content />
         </div>
-        {/* </Scrollbars> */}
       </TracksProvider>
     </LocalizationProvider>
   );
