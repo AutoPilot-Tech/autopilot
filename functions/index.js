626706d2d22174e064ec9637964036219d6caae5
molinar1999 <matthew.a.molinar@gmail.com>
2022-05-17 00:34:05 -0500

    Add express to backend

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const express = require("express");
+const app = express();
 const moment = require("moment");
 const {v4: uuidv4} = require("uuid");
 let useEmulator;
