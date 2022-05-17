86ff0cd18cff5f592cc5d802d25dd2ee21eca2dc
molinar1999 <matthew.a.molinar@gmail.com>
2022-05-17 06:10:03 -0500

    Add routine if no routines

--- src/components/functional/RoutinePicker.js
+++ src/components/functional/RoutinePicker.js
@@ -3,6 +3,8 @@ import React, {useState, useEffect, useRef} from "react";
 import {useTracksValue} from "../../context/tracks-context";
 import {db, auth} from "../../firebase";
 import {AddRoutine} from "../AddRoutine";
+import {getRandomColor} from "../../helpers/index";
+import {generatePushId} from "../../helpers/index";
 
 /**
  * Hook that alerts clicks outside of the passed ref
@@ -64,7 +66,10 @@ export function RoutinePicker({
   routineSetterOpen,
   setRoutineSetterOpen,
 }) {
-  const [tracks, setTracks] = useState([]);
+  const {tracks, setTracks} = useTracksValue();
+  const [routines, setRoutines] = useState([]);
+  const [routineCreator, setRoutineCreator] = useState(false);
+  const [newRoutineName, setNewRoutineName] = useState("");
   useEffect(() => {
     const userId = auth.currentUser.uid;
     const unsubscribe = db
@@ -75,10 +80,45 @@ export function RoutinePicker({
           id: doc.id,
           ...doc.data(),
         }));
-        setTracks(newTracks);
+        setRoutines(newTracks);
       });
     return () => unsubscribe();
   }, []);
+
+  const addTrack = () => {
+    const trackId = generatePushId();
+    let trackColor = getRandomColor();
+    newRoutineName &&
+      db
+        .collection("tracks")
+        .add({
+          trackId,
+          name: newRoutineName,
+          userId: auth.currentUser.uid,
+          routine: false,
+          textColor: `text-${trackColor}-500`,
+          bgColor: `bg-${trackColor}-50`,
+        })
+        .then(() => {
+          tracks.push({
+            trackId,
+            name: newRoutineName,
+            userId: auth.currentUser.uid,
+            routine: false,
+            color: trackColor,
+          });
+          setTracks([...tracks]);
+          setNewRoutineName("");
+        });
+  };
+
+  const handleKeypress = (e) => {
+    //it triggers by pressing the enter key
+    if (e.keyCode === 13) {
+      addTrack();
+    }
+  };
+
   return (
     <div
       className={
@@ -97,16 +137,54 @@ export function RoutinePicker({
         <div className="flex flex-col gap-3">
           {tracks.length === 0 ? (
             <div className="text-center flex-col flex">
-              <div className="text-gray-500">
-                <p>You don't have any routines yet.</p>
-                <br />
-                <button
-        type="button"
-        className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-      >
-        Add one
-      </button>
-              </div>
+              {routineCreator ? (
+                <>
+                  <h4 className="text-md font-medium leading-6 text-gray-900">
+                    New Routine
+                  </h4>
+                  <div className="flex flex-col">
+                    <input
+                      className="mt-3 w-full p-2 rounded-md text-gray-900 placeholder-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out"
+                      data-testid="add-task-content"
+                      type="text"
+                      value={newRoutineName}
+                      onChange={(e) => setNewRoutineName(e.target.value)}
+                      placeholder="Routine Name"
+                      onKeyDown={(e) => handleKeypress(e)}
+                    />
+                    <div className="mt-4 grid grid-cols-2">
+                      <button
+                        type="button"
+                        className="m-auto inline-flex justify-center px-4 py-2 text-sm font-medium text-red-900 bg-red-100 border border-transparent rounded-md hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-red-500"
+                        onClick={() => setShowRoutinesList(false)}
+                      >
+                        Cancel
+                      </button>
+                      <button
+                        type="button"
+                        className="m-auto inline-flex justify-center px-4 py-2 text-sm font-medium text-green-900 bg-green-100 border border-transparent rounded-md hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-green-500"
+                        onClick={() => addTrack()}
+                      >
+                        Add Routine
+                      </button>
+                    </div>
+                  </div>
+                </>
+              ) : (
+                <div className="text-gray-500">
+                  <p>You don't have any routines yet.</p>
+                  <br />
+                  <button
+                    type="button"
+                    className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    onClick={() => {
+                      setRoutineCreator(true);
+                    }}
+                  >
+                    Add one
+                  </button>
+                </div>
+              )}
             </div>
           ) : (
             tracks.map((track) => (
