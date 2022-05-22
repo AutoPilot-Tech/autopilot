ae111d21f680f748c9e258693ef53a2eaa144c4f
molinar1999 <matthew.a.molinar@gmail.com>
2022-05-22 00:50:57 -0500

    Fixed media query for sidebar to max width 480px, apparently phones be big

--- src/context/loading-context.js
+++ src/context/loading-context.js
@@ -12,7 +12,7 @@ export const LoadingProvider = ({children}) => {
   const [openSideBar, setOpenSideBar] = useState(false);
 
   useEffect(() => {
-    const mql = window.matchMedia("(max-width: 425px");
+    const mql = window.matchMedia("(max-width: 480px");
     if (!mql.matches) {
       setOpenSideBar(true);
     }
