cbfea69843455f1b79268d7faeb7480649189a61
molinar1999 <matthew.a.molinar@gmail.com>
2022-05-22 05:04:46 -0500

    added photourl from signed in with google to header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -123,6 +123,49 @@ export function Header() {
             <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
           </button>
         </div>
+        {/* Profile dropdown */}
+        <Menu as="div" className="ml-3 relative">
+          <div>
+            <Menu.Button className="max-w-xs bg-white flex items-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              <span className="sr-only">Open user menu</span>
+              <img
+                className="h-8 w-8 rounded-full"
+                src={auth.currentUser.photoURL}
+                alt=""
+              />
+            </Menu.Button>
+          </div>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-200"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+              {userNavigation.map((item) => (
+                <Menu.Item key={item.name}>
+                  {({active}) => (
+                    <a
+                      href={item.href}
+                      className={classNames(
+                        active ? "bg-gray-100" : "",
+                        "block px-4 py-2 text-sm text-gray-700"
+                      )}
+                      onClick={() => {
+                        item.onClick();
+                      }}
+                    >
+                      {item.name}
+                    </a>
+                  )}
+                </Menu.Item>
+              ))}
+            </Menu.Items>
+          </Transition>
+        </Menu>
         {/* <div className="hidden md:ml-4 md:flex md:items-center">
           <Menu as="div" className="relative">
             <Menu.Button
