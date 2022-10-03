e6d144cf1a7b8770ac81ac29c1b7ab8ef9e7778f
molinar1999 <matthew.a.molinar@gmail.com>
2022-10-03 15:21:53 -0500

    installed gapi-script and react-google-login

--- src/App.js
+++ src/App.js
@@ -15,38 +15,46 @@ import {CalendarMonthView} from "./pages/CalendarMonthView";
 import {TaskView} from "./pages/TaskView";
 import SyncLoader from "react-spinners/SyncLoader";
 import {auth} from "./firebase";
-import {GoogleApiProvider} from "react-gapi";
+import {GoogleLogin} from "react-google-login";
+import {gapi} from "gapi-script";
+
+const clientId =
+  "39033041323-td4qpdmn6t5765rvdev51v68f7qof0pv.apps.googleusercontent.com";
 
 export const App = () => {
+  useEffect(() => {
+    const initClient = () => {
+      gapi.client.init({
+        clientId: clientId,
+        scope: "",
+      });
+    };
+    gapi.load("client:auth2", initClient);
+  });
   return (
-    <GoogleApiProvider>
-      <DndProvider backend={HTML5Backend}>
-        <LoadingProvider>
-          <Router>
-            <Routes>
-              {/* <Route path="/" element={<Landing />} /> */}
-              <Route path="/signup" element={<SignupNew />} />
-              <Route path="/settings" element={<Settings />} />
-              <Route path="/login" element={<LoginNew />} />
-              {/* React Router V6 and above doesn't support optional paths so we have to make several.. */}
-              <Route path="/app/*" element={<CalendarHomeView />} />
-              <Route
-                path="/app/calendar/home/:year/:month/:day"
-                element={<CalendarHomeView />}
-              />
-              <Route path="/app/calendar/week" element={<CalendarWeekView />} />
-              <Route
-                path="/app/calendar/week/:year/:month/:day"
-                element={<CalendarWeekView />}
-              />
-              <Route
-                path="/app/calendar/month"
-                element={<CalendarMonthView />}
-              />
-            </Routes>
-          </Router>
-        </LoadingProvider>
-      </DndProvider>
-    </GoogleApiProvider>
+    <DndProvider backend={HTML5Backend}>
+      <LoadingProvider>
+        <Router>
+          <Routes>
+            {/* <Route path="/" element={<Landing />} /> */}
+            <Route path="/signup" element={<SignupNew />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/login" element={<LoginNew />} />
+            {/* React Router V6 and above doesn't support optional paths so we have to make several.. */}
+            <Route path="/app/*" element={<CalendarHomeView />} />
+            <Route
+              path="/app/calendar/home/:year/:month/:day"
+              element={<CalendarHomeView />}
+            />
+            <Route path="/app/calendar/week" element={<CalendarWeekView />} />
+            <Route
+              path="/app/calendar/week/:year/:month/:day"
+              element={<CalendarWeekView />}
+            />
+            <Route path="/app/calendar/month" element={<CalendarMonthView />} />
+          </Routes>
+        </Router>
+      </LoadingProvider>
+    </DndProvider>
   );
 };
